df514e1da618c38e23df4efbcf477cb6288b009a
Jeremy Banka <hello@jeremybanka.com>
2021-05-28 20:47:04 -0700

    🏷  declare some basic content for topdeck, specify svg

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,6 +1,40 @@
 /* eslint-disable quotes */
 
+// type Privacy = `public` | `hidden` | `secret`
+
+declare interface card {
+  id: string
+  content: unknown
+  ownedBy: string | null
+  livesIn: string | null
+  privacy: string
+  rotated: number
+}
+
+declare interface deck {
+  id: string
+  cards: card[]
+  ownedBy: string | null
+  livesIn: string | null
+  privacy: string
+  rotated: number
+  shuffle: VoidFunction
+}
+
+declare interface deckOptions {
+  _deck: deck
+  shuffle: Dispatch<SetStateAction<undefined>>
+  usePosition?: bool
+}
+
+declare class ReactInputPosition {
+  render(): JSX.Element
+}
+
 declare module "*.svg" {
-  const content: any
-  export default content
+  import React = require("react")
+
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>
+  const src: string
+  export default src
 }
